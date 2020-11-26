468bba9ae01501c1ce2470674712d629721788f4
Viktor Pera <peviktor@hotmail.com>
2020-11-26 16:11:21 +0100

    eerste commit cms devlp

--- src/components/header.js
+++ src/components/header.js
@@ -16,17 +16,17 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h3 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `black`,
             textDecoration: `none`,
           }}
         >
           {siteTitle}
         </Link>
-      </h1>
+      </h3>
     </div>
   </header>
 )
