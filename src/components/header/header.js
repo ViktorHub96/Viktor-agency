8a2c527ea6f5317cee857713c191d8a265a79029
Viktor Pera <peviktor@hotmail.com>
2020-12-27 17:07:01 +0100

    Commit

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,35 +1,11 @@
-import { Link } from "gatsby"
+import { useStaticQuery,graphql } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h3 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `black`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h3>
-    </div>
-  </header>
-)
+const Header = ({ siteTitle }) => {
+ 
+  return <div>Header</div>
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 
-import SEO from "../components/seo"
+import SEO from "../components/Seo"
 
 const IndexPage = () => (
   <Layout>
